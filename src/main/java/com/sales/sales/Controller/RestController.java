aad474bf0db10f0e1989d97ed3429d8b57f4281c
Rıfkı Kesepara <rifkikesepara7@gmail.com>
2025-05-20 11:19:55 +0300

    Kubernates deployment

--- src/main/java/com/sales/sales/Controller/RestController.java
+++ src/main/java/com/sales/sales/Controller/RestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RestController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Rıfkı Kesepara";
     }
 }
